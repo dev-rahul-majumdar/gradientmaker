6ea88806556217c01fc4d6ccb66967d9a19c7a79
Rahul Majumdar <rahul.majumdar@outlook.com>
2020-06-15 22:09:27 -0400

    added build in gh-pages branch

---
+++ build/precache-manifest.6ccff825b2b80db4afd51509da2c0c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2694d153b7ad6337161473e06e06c5d",
+    "url": "/gradientmaker/index.html"
+  },
+  {
+    "revision": "d9a601a39915cf8afc18",
+    "url": "/gradientmaker/static/css/main.18f5af27.chunk.css"
+  },
+  {
+    "revision": "b7a152a5edfa92b7453b",
+    "url": "/gradientmaker/static/js/2.f6613d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gradientmaker/static/js/2.f6613d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a601a39915cf8afc18",
+    "url": "/gradientmaker/static/js/main.5caece7f.chunk.js"
+  },
+  {
+    "revision": "355bcf088e5d54263bf2",
+    "url": "/gradientmaker/static/js/runtime-main.37516407.js"
+  }
+]);
