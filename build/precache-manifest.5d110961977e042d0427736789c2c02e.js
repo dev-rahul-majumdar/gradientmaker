adaf9baaefdeb20d8180e6d703c93049acf9415c
Rahul Majumdar <rahul.majumdar@outlook.com>
2020-06-18 11:53:49 -0400

    added files to gh-pages

---
+++ build/precache-manifest.5d110961977e042d0427736789c2c02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfcf2dd29a92747b1a677e988e224cb",
+    "url": "/gradientmaker/build/index.html"
+  },
+  {
+    "revision": "8246f01c5b314e48b4fd",
+    "url": "/gradientmaker/build/static/css/main.18f5af27.chunk.css"
+  },
+  {
+    "revision": "b7a152a5edfa92b7453b",
+    "url": "/gradientmaker/build/static/js/2.f6613d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gradientmaker/build/static/js/2.f6613d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8246f01c5b314e48b4fd",
+    "url": "/gradientmaker/build/static/js/main.5ac2db39.chunk.js"
+  },
+  {
+    "revision": "144b5d4cb3c27332fce2",
+    "url": "/gradientmaker/build/static/js/runtime-main.4aca5b57.js"
+  }
+]);
