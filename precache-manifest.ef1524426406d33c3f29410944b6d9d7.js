c701a80e943746fb2f0c007ab0cee63dd6c6e92f
Rahul Majumdar <rahul.majumdar@outlook.com>
2020-06-19 15:47:09 -0400

    add files via upload

---
+++ precache-manifest.ef1524426406d33c3f29410944b6d9d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f35b21c16e21ca4447ec73516f0696f",
+    "url": "/gradientmaker/index.html"
+  },
+  {
+    "revision": "e5b7cad4b83d690b7e5e",
+    "url": "/gradientmaker/static/css/main.073aff76.chunk.css"
+  },
+  {
+    "revision": "b7a152a5edfa92b7453b",
+    "url": "/gradientmaker/static/js/2.f6613d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gradientmaker/static/js/2.f6613d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b7cad4b83d690b7e5e",
+    "url": "/gradientmaker/static/js/main.5caece7f.chunk.js"
+  },
+  {
+    "revision": "355bcf088e5d54263bf2",
+    "url": "/gradientmaker/static/js/runtime-main.37516407.js"
+  }
+]);
